384c509ed956bb57856ed18d9f0bc40e2da69700
BlaneTorregrossa <blanet97@cox.net>
2018-02-28 12:02:18 -0600

    went back and forth on a lot of stuff
    Working more on this tonight hopefully

--- conjuntion.py
+++ conjuntion.py
@@ -1,19 +1,28 @@
-class conjuntion(object):
-    def _init_ (self, variables, clauses, numvariables, numclauses):
-        self.variables = variables
-        self.clauses = clauses
-        self.numvariables = numvariables
-        self.numclauses = numclauses
-        self.avaliblechar = []
+class _conjuntion(object):
+    def __init__(self):
+        self.variables = []
+        self.clauses = []
+        self.numvariables = []
+        self.numclauses = []
+        self.pair = []
+        self.target = ""
+        self.vara = ""
+        self._settarget()
+        self._setvariables(self.vara)
+       # self._setclauses()
+       # self._setclausespair()
+
+
+    def _settarget(self):
+        self.target = "Hello World!"
+
+    def _setvariables(self, currentvar):
+        prevvar = currentvar
+        self.variables = currentvar
+        if prevvar == currentvar:
+            prevvar = ""
+        
+    def _setclauses(self, var):
         
 
-    # for limiting selection
-    def setavaliblechar(self):
-        
-  
-
-    def settarget(self):
-   
-
-
-
+    #def _setclausepair(self):
